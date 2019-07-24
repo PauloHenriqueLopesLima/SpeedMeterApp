d874315ada8c7822e6884600d5c29aa3274ce2f8
Paulo Henrique <neomatrixgames@gmail.com>
2019-07-23 21:18:56 -0300

    ajustado algumas coisas referente as validaçoes de localização do usuario.

--- app/src/main/java/com/example/speedmeterapp/LocationService.java
+++ app/src/main/java/com/example/speedmeterapp/LocationService.java
@@ -7,26 +7,29 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import androidx.annotation.Nullable;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-import java.text.DecimalFormat;
 
-public class LocationService extends Service implements LocationListener,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
+
+import static com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY;
+
+public class LocationService extends Service implements LocationListener,GoogleApiClient.ConnectionCallbacks {
  
     private static final long INTERVAL = 500;
     private static final long FASTEST_INTERVAL = 500;
     private LocationRequest LocationRequest;
     private GoogleApiClient GoogleApiClient;
-    private Location location, pontoA, pontoB;
+    private Location pontoA;
+    private Location pontoB;
     static double distancia = 0;
     double velocidade;
 
+
     private final IBinder binder = new LocalBinder();
- 
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -34,7 +37,6 @@ public class LocationService extends Service implements LocationListener,GoogleA
         GoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
                 .build();
         GoogleApiClient.connect();
         return binder;
@@ -44,7 +46,7 @@ public class LocationService extends Service implements LocationListener,GoogleA
         LocationRequest = new LocationRequest();
         LocationRequest.setInterval(INTERVAL);
         LocationRequest.setFastestInterval(FASTEST_INTERVAL);
-        LocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationRequest.setPriority(PRIORITY_HIGH_ACCURACY);
     }
 
     @Override
@@ -58,6 +60,7 @@ public class LocationService extends Service implements LocationListener,GoogleA
             LocationServices.FusedLocationApi.requestLocationUpdates(
                     GoogleApiClient, LocationRequest, this);
         } catch (SecurityException e) {
+            e.printStackTrace();
         }
     }
      @Override
@@ -66,46 +69,32 @@ public class LocationService extends Service implements LocationListener,GoogleA
 
     @Override
     public void onLocationChanged(Location location) {
-        MainActivity.locate.dismiss();
-        this.location = location;
+
         if (pontoA == null) {
-            pontoA = this.location;
-            pontoB = this.location;
+            pontoA = location;
+            pontoB = location;
         } else
-            pontoB = this.location;
- 
-        atualizarUI();
-
-        velocidade = location.getSpeed() * 18 / 5 ;
+            pontoB = location;
+         atualizarUI();
+        velocidade = location.getSpeed() * 3.6 ;
      }
- 
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
+    class LocalBinder extends Binder {
 
-     }
- 
-    public class LocalBinder extends Binder {
-         public LocationService getService() {
+         LocationService getService() {
             return LocationService.this;
         }
      }
- 
+
     private void atualizarUI() {
         if (MainActivity.p == 0) {
             distancia = distancia + (pontoA.distanceTo(pontoB) / 1000.00);
             if (velocidade > 0)
-                MainActivity.speed.setText("Sua velocidade"+ new DecimalFormat("##,##").format(velocidade)  + " km/hr");
             MainActivity.digitSpeedView.updateSpeed((int) velocidade);
-
         }
-
             else {
-            MainActivity.speed.setText("parado");
-
             pontoA = pontoB;
         }
-         }
-
+    }
     @Override
     public boolean onUnbind(Intent intent) {
          if (GoogleApiClient.isConnected())
--- app/src/main/java/com/example/speedmeterapp/MainActivity.java
+++ app/src/main/java/com/example/speedmeterapp/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.speedmeterapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.IntentSender;
 import android.content.pm.ActivityInfo;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Button;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.ComponentName;
@@ -14,99 +21,108 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.github.capur16.digitspeedviewlib.DigitSpeedView;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
 
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-        LocationService Service;
-        static DigitSpeedView digitSpeedView;
-        static boolean status;
-        LocationManager locationManager;
-        ImageView image;
-        static ProgressDialog locate;
-        static int p = 0;
-        static TextView speed;
+    static LocationService locationService;
+    static DigitSpeedView digitSpeedView;
+    static boolean status;
+    ImageView image;
+    static int p = 0;
+    Button sinalButton;
+    private LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+   String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+        String rationale = "Para usar este App é necessario permitir a sua localização. ...";
+        Permissions.Options options = new Permissions.Options()
+                .setRationaleDialogTitle("Serviço de localização")
+                .setSettingsDialogTitle("Localização");
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+       Permissions.check(this, permissions, rationale, options, new PermissionHandler() {
+            @Override
+            public void onGranted() {
 
-            checkGps();
-            startGps();
-
-            speed = findViewById(R.id.speedtext);
-            image = findViewById(R.id.image);
-            digitSpeedView = findViewById(R.id.digitSpeedView);
-
-
-
-        }
-        private void checkGps () {
-            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-
-
-                showGPSDisabledAlertToUser();
+                ativarGps();
+                verificarLocalizacao();
+                ativarGps();
             }
-        }
-        private void startGps ()
-            {
-                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                    return;
-                }
-                if (status == false)
-                    bindService();
-                locate = new ProgressDialog(MainActivity.this);
-                locate.setIndeterminate(true);
-                locate.setCancelable(false);
-                locate.setMessage("Recebendo dados de sua localização");
-                locate.show();
+            @Override
+            public void onDenied(Context context, ArrayList<String> deniedPermissions) {
+                finish();
             }
-        private void stopGps () {
-            if (status == true)
-                unbindService();
-            p = 0;
-        }
+        });
+
+        image = findViewById(R.id.image);
+        digitSpeedView = findViewById(R.id.digitSpeedView);
+        sinalButton = findViewById(R.id.sinal_button);
+        ativarGps();
+    }
 
 
-        private void showGPSDisabledAlertToUser () {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-                alertDialogBuilder.setMessage("Deseja Ativar o GPS ?")
-                        .setCancelable(false)
-                        .setPositiveButton("Ativar GPS",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        Intent callGPSSettingIntent = new Intent(
-                                                android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                                        startActivity(callGPSSettingIntent);
-                                    }
-                                });
-                alertDialogBuilder.setNegativeButton("Cancelar",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-                AlertDialog alert = alertDialogBuilder.create();
+    private void verificarLocalizacao() {
+
+            LocationManager locationManager = null;
+            boolean gps_enabled = false;
+            boolean network_enabled = false;
+            if ( locationManager == null ) {
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            }
+            try {
+                gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            } catch (Exception ex){}
+            try {
+                network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            } catch (Exception ex){}
+            if ( !gps_enabled && !network_enabled ){
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setMessage("A função de localização, não esta ativada.");
+                dialog.setPositiveButton("Ativar", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+                        ativarGps();
+                    }
+                });
+
+                AlertDialog alert = dialog.create();
                 alert.show();
+                ativarGps();
             }
 
+        ativarGps();
+        }
             private ServiceConnection serviceConnection = new ServiceConnection() {
                 @Override
                 public void onServiceConnected(ComponentName name, IBinder service) {
                     LocationService.LocalBinder binder = (LocationService.LocalBinder) service;
-                    Service = binder.getService();
+                    locationService = binder.getService();
                     status = true;
                 }
 
@@ -116,45 +132,64 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
-
-            void bindService () {
-                if (status == true)
+            void ativarGps() {
+                if (status)
                     return;
                 Intent i = new Intent(getApplicationContext(), LocationService.class);
                 bindService(i, serviceConnection, BIND_AUTO_CREATE);
                 status = true;
-
             }
 
-            void unbindService () {
-                if (status == false)
+            void desativarGps() {
+                if (!status)
                     return;
                 Intent i = new Intent(getApplicationContext(), LocationService.class);
                 unbindService(serviceConnection);
                 status = false;
             }
-            @Override
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ativarGps();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        ativarGps();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        ativarGps();
+    }
+
+    @Override
             protected void onResume () {
                 super.onResume();
+                ativarGps();
             }
             @Override
             protected void onStart () {
                 super.onStart();
+                ativarGps();
             }
             @Override
             protected void onDestroy () {
                 super.onDestroy();
-                if (status == true)
-                    unbindService();
+                if (status)
+                    desativarGps();
             }
             @Override
             public void onBackPressed () {
-                if (status == false)
+                if (!status)
                     super.onBackPressed();
                 else
                     moveTaskToBack(true);
             }
-        }
 
 
 
+}
